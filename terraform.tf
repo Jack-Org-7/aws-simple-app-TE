b8ae60120a586a745daed8be5a3efb15f97401fb
Jack Valley <jvalley19@gmail.com>
2023-10-28 13:54:34 -0400

    init terr

--- terraform.tf
+++ terraform.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "~> 3.28.0"
+        version = "~> 4.45.0"
     }
   }
   required_version = ">= 1.1.0"
