ed75e5b08e5009192f427804ee4af5971f22f53d
Jack Valley <jvalley19@gmail.com>
2023-10-28 13:51:29 -0400

    new updates

--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,17 @@
 terraform {
-    cloud {
-      organization = "ACME_DEMO_01"
+  cloud {
+    organization = "ACME_DEMO_01"
 
-      workspaces {
-        name = "aws-simple-app-TE"
-      }
-  }
-    required_providers {
-      aws = {
-          source = "hashicorp/aws"
-          version = "~> 3.28.0"
-      }
+    workspaces {
+      name = "aws-simple-app-TE"
     }
-    required_version = ">= 1.1.0"
+  }
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.28.0"
+    }
+  }
+  required_version = ">= 1.1.0"
 
 }
