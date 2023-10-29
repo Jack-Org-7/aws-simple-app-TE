8e3970198e8db5d64dc43e59213f0ea785237786
Jack Valley <jvalley19@gmail.com>
2023-10-29 11:20:35 -0400

    nu

--- main.tf
+++ main.tf
@@ -121,8 +121,7 @@ resource "aws_lb_listener" "listener" {
 
 resource "aws_ecs_service" "app_service" {
   name            = "mtn-demo-service"                            
-  cluster         = "${aws_ecs_cluster.app_cluster
-.id}"            
+  cluster         = "${aws_ecs_cluster.app_cluster.id}"            
   task_definition = "${aws_ecs_task_definition.app_task.arn}" 
   launch_type     = "FARGATE"
   desired_count   = 3 
